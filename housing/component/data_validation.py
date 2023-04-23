e4cdc7310b04bc6227ca6a5d83c9c5f7426ce6b4
Ganesh <ganeshkumarjasti19@gmail.com>
2023-04-23 17:42:15 +0530

    updated

--- housing/component/data_validation.py
+++ housing/component/data_validation.py
@@ -20,6 +20,7 @@ class DataValidation:
     def __init__(self,data_validation_config:DataValidationConfig,
         data_ingestion_artifact:DataIngestionArtifact):
         try:
+            logging.info(f"{'>>'*30}Data Validation log started.{'<<'*30} \n\n")
             self.data_validation_config = data_validation_config
             self.data_ingestion_artifact = data_ingestion_artifact
         except Exception as e:
@@ -132,5 +133,10 @@ class DataValidation:
                 message="Data Validation performed Successfully."
             )# type: ignore
             logging.info(f"Data Validation artifact:{data_validation_artifact}")
+            return data_validation_artifact
         except Exception as e: 
             raise HousingException(e,sys) from e
+    
+
+    def __del__(self):
+        logging.info(f"{'>>'*30}Data Validation log completed.{'<<'*30} \n\n")
